b88ba67bc9b7c769468e7e694300bcc9edd83604
Abdurrahman Jahankeer <abdurrahman.jahankeer@ontariotechu.net>
2025-11-18 17:37:58 -0500

    I finished public/Script/app.js

---
+++ public/Script/app.js
@@ -0,0 +1,8 @@
+// IIFE or Immediately invoked function expression
+(function(){
+    function start(){
+        console.log("App started...")
+        console.log("Assignment Tracker loaded!");
+    }
+    window.addEventListener("load",start);
+})();

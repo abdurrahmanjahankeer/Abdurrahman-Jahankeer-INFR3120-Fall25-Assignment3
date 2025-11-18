8dd9d4e34f5af99e0bdd9ccb9d87a162c313aa6b
Abdurrahman Jahankeer <abdurrahman.jahankeer@ontariotechu.net>
2025-11-18 11:22:06 -0500

    added header, main_nav and footer

--- server/config/app.js
+++ server/config/app.js
@@ -6,22 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
+var assignmentsRouter = require('../routes/assignments');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../../server/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+// ROUTERS
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/assignments', assignmentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,11 +33,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });

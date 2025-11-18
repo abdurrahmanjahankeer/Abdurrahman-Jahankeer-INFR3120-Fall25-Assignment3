0377c4b57e867055b4b8333eaa36ed216f6f03b7
Abdurrahman Jahankeer <abdurrahman.jahankeer@ontariotechu.net>
2025-11-18 18:35:13 -0500

    I finished all of the comments

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 #!/usr/bin/env node
 
-// --- Imports and App Setup
+// This loads secret settings (like your database link) from .env so your app can use them
 require('dotenv').config();
+
+// This grabs my main Express app from the config folder
 var app = require('./server/config/app');
+
+// This sets up a way to print useful debug messages when my app runs
 var debug = require('debug')('abdurrahman-jahankeer-infr3120-fall25-assignment3:server');
+
+// This brings in Node's HTTP server so people can visit my website
 var http = require('http');
 
-// --- Port Setup
+// This picks which port my website will use (3000 unless I say otherwise)
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// --- HTTP Server Setup
+// This actually creates the real web server using my app
 var server = http.createServer(app);
 
+// Start my server listening for people who visit the site
 server.listen(port);
+
+// Handle what happens if there's an error or when the server is ready
 server.on('error', onError);
 server.on('listening', onListening);
 
-// --- Utility Functions
+// This function makes sure the port number is safe and correct
 function normalizePort(val) {
   var port = parseInt(val, 10);
   if (isNaN(port)) return val;
@@ -25,6 +34,7 @@ function normalizePort(val) {
   return false;
 }
 
+// This function shows an error if something goes wrong (like if the port is busy)
 function onError(error) {
   if (error.syscall !== 'listen') throw error;
   var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
@@ -42,6 +52,7 @@ function onError(error) {
   }
 }
 
+// This function prints out a nice message when my server is up and running
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
